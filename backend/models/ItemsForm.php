c7d6521902f3f82afe9500dd8974b14989a94172
quang.nh94 <quang.nh94@gmail.com>
2016-02-24 17:00:48 +0700

    commit data

--- backend/models/ItemsForm.php
+++ backend/models/ItemsForm.php
@@ -6,13 +6,30 @@ use yii\base\Model;
 
 class ItemsForm extends Model {
 
-    
+    public $id;
+    public $item_name;
+    public $description;
+    public $content;
+    public $created_at;
+    public $updated_at;
+    public $alias;
+    public $active;
+    public $category_id;
+    public $root_price;
+    public $sell_price;
+    public $quantity;
+    public $sold_quantity;
+    public $user_id;
+    public $images;
 
     public function rules() {
         return [
-            [['email', 'password'], 'required', 'message' => '{attribute} không được để trống'],
-            ['email', 'email'],
-            [['password'], 'string']
+            [['item_name', 'description', 'content', 'created_at', 'updated_at', 'alias', 'category_id', 'root_price', 'sell_price', 'quantity', 'sold_quantity'], 'required'],
+            [['content'], 'string'],
+            [['created_at', 'updated_at', 'active', 'category_id', 'quantity', 'sold_quantity', 'user_id'], 'integer'],
+            [['root_price', 'sell_price'], 'number'],
+            [['item_name', 'description'], 'string', 'max' => 350],
+            [['alias'], 'string', 'max' => 500],
         ];
     }
 
